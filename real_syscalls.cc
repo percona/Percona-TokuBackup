cde3681c48343e158745d568e9952d1efd60affc
Christian Rober <christianrober@gmail.com>
2013-02-05 05:30:19 +0000

    refs #5599 adding function declarations for weird compiler errors.

--- real_syscalls.cc
+++ real_syscalls.cc
@@ -18,6 +18,7 @@
 // **************************************************************************
 
 static int (*real_open)(const char *file, int oflag, ...) = NULL;
+int call_real_open(const char *file, int oflag, ...);
 int call_real_open(const char *file, int oflag, ...) {
     if (real_open == NULL) {
         // really should have a mutex here to protect this.
@@ -38,6 +39,7 @@ int call_real_open(const char *file, int oflag, ...) {
 }
 
 static int (*real_close)(int fd) = NULL;
+int call_real_close(int fd);
 int call_real_close(int fd) {
     if (real_close == NULL) {
         real_close = (int(*)(int))dlsym(RTLD_NEXT, "close");
@@ -48,6 +50,7 @@ int call_real_close(int fd) {
 }
 
 static ssize_t (*real_write)(int fd, const void *buf, size_t nbyte) = NULL;
+ssize_t call_real_write(int fd, const void *buf, size_t nbyte);
 ssize_t call_real_write(int fd, const void *buf, size_t nbyte) {
     if (real_write==NULL) {
         real_write = (ssize_t(*)(int,const void*,size_t))dlsym(RTLD_NEXT, "write");
@@ -57,6 +60,7 @@ ssize_t call_real_write(int fd, const void *buf, size_t nbyte) {
 }
 
 static ssize_t (*real_read)(int fildes, const void *buf, size_t nbyte) = NULL;
+ssize_t call_real_read(int fildes, const void *buf, size_t nbyte);
 ssize_t call_real_read(int fildes, const void *buf, size_t nbyte) {
     if (real_read == NULL) {
         real_read = (ssize_t(*)(int,const void*,size_t))dlsym(RTLD_NEXT, "read");
@@ -66,6 +70,7 @@ ssize_t call_real_read(int fildes, const void *buf, size_t nbyte) {
 }
 
 static ssize_t (*real_pwrite)(int fildes, const void *buf, size_t nbyte, off_t offset) = NULL;
+ssize_t call_real_pwrite(int fildes, const void *buf, size_t nbyte, off_t offset);
 ssize_t call_real_pwrite(int fildes, const void *buf, size_t nbyte, off_t offset) {
     if (real_pwrite == NULL) {
         real_pwrite = (ssize_t(*)(int,const void*,size_t,off_t))dlsym(RTLD_NEXT, "pwrite");
@@ -75,6 +80,7 @@ ssize_t call_real_pwrite(int fildes, const void *buf, size_t nbyte, off_t offset
 }
 
 static int (*real_ftruncate)(int fildes,  off_t length) = NULL;
+int call_real_ftruncate(int fildes, off_t length);
 int call_real_ftruncate(int fildes, off_t length) {
     if (real_ftruncate == NULL) {
         real_ftruncate = (int(*)(int,off_t))dlsym(RTLD_NEXT, "ftruncate");
@@ -84,6 +90,7 @@ int call_real_ftruncate(int fildes, off_t length) {
 }
 
 static int (*real_truncate)(const char *path, off_t length) = NULL;
+int call_real_truncate(const char *path, off_t length);
 int call_real_truncate(const char *path, off_t length) {
     if (real_truncate == NULL) {
         real_truncate = (int(*)(const char *,off_t))dlsym(RTLD_NEXT, "truncate");
@@ -93,6 +100,7 @@ int call_real_truncate(const char *path, off_t length) {
 }
 
 static int (*real_unlink)(const char* path) = NULL;
+int call_real_unlink(const char *path);
 int call_real_unlink(const char *path) {
     if (real_unlink == NULL) {
         real_unlink = (int(*)(const char*))dlsym(RTLD_NEXT, "unlink");
@@ -102,6 +110,7 @@ int call_real_unlink(const char *path) {
 }
 
 static int (*real_rename)(const char *oldpath, const char *newpath) = NULL;
+int call_real_rename(const char* oldpath, const char* newpath);
 int call_real_rename(const char* oldpath, const char* newpath) {
     // TODO: Isn't there a way to set this during backup initialization?
     // Especially if we protect interposition with locking of the backup
