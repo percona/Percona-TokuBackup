0dec61f7be2868aefc77692b74fa08a2736c722b
Bradley C. Kuszmaul <bradley@tokutek.com>
2013-03-03 20:26:37 +0000

    Refs #6148.  Fix dlsym races.  {{{svn merge -c 53901 https://svn.tokutek.com/tokudb/mysql.branches/6.6.4.backup/tokudb/backup}}}

--- real_syscalls.cc
+++ real_syscalls.cc
@@ -8,8 +8,27 @@
 #include <unistd.h>
 #include <pthread.h>
 
+#include "real_syscalls.h"
+
 static pthread_mutex_t dlsym_mutex = PTHREAD_MUTEX_INITIALIZER;
 
+template <class T> static void dlsym_set(T *ptr, const char *name)
+// Effect: lookup up NAME using dlsym, and store the result into *PTR.  Do it atomically, using dlsym_mutex for mutual exclusion.
+// Rationale:  There were a whole bunch of these through this code, and they all are the same except the type.  Rather than
+//   programming it with macros, I do it in a type-safe way with templates. -Bradley
+{
+    if (*ptr==NULL) {
+        pthread_mutex_lock(&dlsym_mutex);
+        if (*ptr==NULL) {
+            // the pointer is still NULL, so do the set,  otherwise someone else changed it while I held the pointer.
+            T *ptr_local = (T*)(dlsym(RTLD_NEXT, name));
+            assert(ptr_local != NULL);
+            bool did_it = __sync_bool_compare_and_swap(ptr, NULL, ptr_local);
+            assert(did_it);
+        }
+        pthread_mutex_unlock(&dlsym_mutex);
+    }
+}
 
 // **************************************************************************
 //
@@ -21,17 +40,11 @@ static pthread_mutex_t dlsym_mutex = PTHREAD_MUTEX_INITIALIZER;
 //
 // **************************************************************************
 
-static int (*real_open)(const char *file, int oflag, ...) = NULL;
-int call_real_open(const char *file, int oflag, ...);
 int call_real_open(const char *file, int oflag, ...) {
-    if (real_open == NULL) {
-        pthread_mutex_lock(&dlsym_mutex);
-        int (*real_open_local)(const char *, int, ...) = (int (*)(const char *, int, ...))dlsym(RTLD_NEXT, "open");
-        assert(real_open_local != NULL);
-        bool did_it = __sync_bool_compare_and_swap(&real_open, NULL, real_open_local);
-        assert(did_it);
-        pthread_mutex_unlock(&dlsym_mutex);
-    }
+    // Put this static decl inside so that someone else cannot use it by mistake.
+    static int (*real_open)(const char *file, int oflag, ...) = NULL; 
+
+    dlsym_set(&real_open, "open");
 
     // See if we are creating or just opening the file.
     if (oflag & O_CREAT) {
@@ -45,104 +58,56 @@ int call_real_open(const char *file, int oflag, ...) {
     }
 }
 
-static int (*real_close)(int fd) = NULL;
-int call_real_close(int fd);
 int call_real_close(int fd) {
-    if (real_close == NULL) {
-        pthread_mutex_lock(&dlsym_mutex);
-        int (*real_close_local)(int) = (int(*)(int))dlsym(RTLD_NEXT, "close");
-        assert(real_close_local != NULL);
-        bool did_it = __sync_bool_compare_and_swap(&real_close, NULL, real_close_local);
-        assert(did_it);
-        pthread_mutex_unlock(&dlsym_mutex);
-    }
-
+    static int (*real_close)(int fd) = NULL;
+    dlsym_set(&real_close, "close");
     return real_close(fd);
 }
 
-static ssize_t (*real_write)(int fd, const void *buf, size_t nbyte) = NULL;
-ssize_t call_real_write(int fd, const void *buf, size_t nbyte);
 ssize_t call_real_write(int fd, const void *buf, size_t nbyte) {
-    if (real_write==NULL) {
-        pthread_mutex_lock(&dlsym_mutex);
-        ssize_t (*real_write_local)(int,const void*,size_t) = (ssize_t(*)(int,const void*,size_t))dlsym(RTLD_NEXT, "write");
-        assert(real_write_local != NULL);
-        bool did_it = __sync_bool_compare_and_swap(&real_write, NULL, real_write_local);
-        assert(did_it);
-        pthread_mutex_unlock(&dlsym_mutex);
-    }
+    static ssize_t (*real_write)(int fd, const void *buf, size_t nbyte) = NULL;
+    dlsym_set(&real_write, "write");
     return real_write(fd, buf, nbyte);
 }
 
-static ssize_t (*real_read)(int fildes, const void *buf, size_t nbyte) = NULL;
-ssize_t call_real_read(int fildes, const void *buf, size_t nbyte);
 ssize_t call_real_read(int fildes, const void *buf, size_t nbyte) {
-    if (real_read == NULL) {
-        real_read = (ssize_t(*)(int,const void*,size_t))dlsym(RTLD_NEXT, "read");
-        assert(real_read != NULL);
-    }
+    static ssize_t (*real_read)(int fildes, const void *buf, size_t nbyte) = NULL;
+    dlsym_set(&real_read, "read");
     return real_read(fildes, buf, nbyte);
 }
 
-static ssize_t (*real_pwrite)(int fildes, const void *buf, size_t nbyte, off_t offset) = NULL;
-ssize_t call_real_pwrite(int fildes, const void *buf, size_t nbyte, off_t offset);
 ssize_t call_real_pwrite(int fildes, const void *buf, size_t nbyte, off_t offset) {
-    if (real_pwrite == NULL) {
-        real_pwrite = (ssize_t(*)(int,const void*,size_t,off_t))dlsym(RTLD_NEXT, "pwrite");
-        assert(real_pwrite != NULL);
-    }
+    static ssize_t (*real_pwrite)(int fildes, const void *buf, size_t nbyte, off_t offset) = NULL;
+    dlsym_set(&real_pwrite, "pwrite");
     return real_pwrite(fildes, buf, nbyte, offset);
 }
 
-static int (*real_ftruncate)(int fildes,  off_t length) = NULL;
-int call_real_ftruncate(int fildes, off_t length);
 int call_real_ftruncate(int fildes, off_t length) {
-    if (real_ftruncate == NULL) {
-        real_ftruncate = (int(*)(int,off_t))dlsym(RTLD_NEXT, "ftruncate");
-        assert(real_ftruncate);
-    }
+    static int (*real_ftruncate)(int fildes,  off_t length) = NULL;
+    dlsym_set(&real_ftruncate, "ftruncate");
     return real_ftruncate(fildes, length);
 }
 
-static int (*real_truncate)(const char *path, off_t length) = NULL;
-int call_real_truncate(const char *path, off_t length);
 int call_real_truncate(const char *path, off_t length) {
-    if (real_truncate == NULL) {
-        real_truncate = (int(*)(const char *,off_t))dlsym(RTLD_NEXT, "truncate");
-        assert(real_truncate);
-    }
+    static int (*real_truncate)(const char *path, off_t length) = NULL;
+    dlsym_set(&real_truncate, "truncate");
     return real_truncate(path, length);
 }
 
-static int (*real_unlink)(const char* path) = NULL;
-int call_real_unlink(const char *path);
 int call_real_unlink(const char *path) {
-    if (real_unlink == NULL) {
-        real_unlink = (int(*)(const char*))dlsym(RTLD_NEXT, "unlink");
-        assert(real_unlink);
-    }
+    static int (*real_unlink)(const char* path) = NULL;
+    dlsym_set(&real_unlink, "unlink");
     return real_unlink(path);
 }
 
-static int (*real_rename)(const char *oldpath, const char *newpath) = NULL;
-int call_real_rename(const char* oldpath, const char* newpath);
 int call_real_rename(const char* oldpath, const char* newpath) {
-    // TODO: Isn't there a way to set this during backup initialization?
-    // Especially if we protect interposition with locking of the backup
-    // data structures...
-    if (real_rename == NULL) {
-        real_rename = (int(*)(const char *,const char *))dlsym(RTLD_NEXT, "rename");
-       assert(real_rename != NULL);
-    }
+    static int (*real_rename)(const char *oldpath, const char *newpath) = NULL;
+    dlsym_set(&real_rename, "rename");
     return real_rename(oldpath, newpath);
 }
 
-static int (*real_mkdir)(const char *pathname, mode_t mode) = NULL;
-int call_real_mkdir(const char *pathname, mode_t mode);
 int call_real_mkdir(const char *pathname, mode_t mode) {
-    if (real_mkdir == NULL) {
-        real_mkdir = (int(*)(const char*, mode_t))dlsym(RTLD_NEXT, "mkdir");
-        assert(real_mkdir);
-    }
+    static int (*real_mkdir)(const char *pathname, mode_t mode) = NULL;
+    dlsym_set(&real_mkdir, "mkdir");
     return real_mkdir(pathname, mode);
 }
