6950bd284fa607ef7ca6c74b0b31168c6a91feb3
Bradley C. Kuszmaul <bradley@tokutek.com>
2013-03-24 15:44:05 +0000

    Refs #6301.  Forgot to add backup_community.c. {{{svn merge -c54659 https://svn.tokutek.com/tokudb/mysql.branches/6.6.4.backup.6302/backup}}}

---
+++ backup_community.cc
@@ -0,0 +1,21 @@
+/* -*- mode: C++; c-basic-offset: 4; indent-tabs-mode: nil -*- */
+// vim: ft=cpp:expandtab:ts=8:sw=4:softtabstop=4:
+
+#include "backup.h"
+#include <errno.h>
+#include <stdio.h>
+
+extern "C" int tokubackup_create_backup(const char *source_dirs[]  __attribute__((unused)),
+                                        const char *dest_dirs[]    __attribute__((unused)),
+                                        int dir_count              __attribute__((unused)),
+                                        backup_poll_fun_t poll_fun __attribute__((unused)),
+                                        void *poll_extra           __attribute__((unused)),
+                                        backup_error_fun_t error_fun,
+                                        void *error_extra) {
+    error_fun(ENOSYS, "Sorry, backup is not implemented", error_extra);
+    return ENOSYS;
+}
+
+extern "C" void tokubackup_throttle_backup(unsigned long bytes_per_second __attribute__((unused))) {
+    fprintf(stderr, "Sorry, backup is not implemented\n");
+}
