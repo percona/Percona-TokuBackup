e110a64f575906e8ada30ac5c013e9b2cee0f7b0
Bradley C. Kuszmaul <bradley@tokutek.com>
2013-03-26 21:27:10 +0000

    Refs #6363.  freed it one line too early.

--- backup_manager.cc
+++ backup_manager.cc
@@ -142,8 +142,8 @@ int backup_manager::prepare_directories_for_backup(backup_session &session)
 
         char * file_name = session.translate_prefix(source_path);
         file->prepare_for_backup(file_name);
-        free(file_name);
         int r = open_path(file_name);
+        free(file_name);
         if (r != 0) {
             // TODO: Could not open path, abort backup.
             session.abort();
