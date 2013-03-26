5ba906313f33cd082a6a0e117f3375c8a03a9302
Christian Rober <christianrober@gmail.com>
2013-03-26 15:09:37 +0000

    refs #6327 #6185 Forgot to include new header file.

---
+++ backup_internal.h
@@ -0,0 +1,16 @@
+/* -*- mode: C++; c-basic-offset: 4; indent-tabs-mode: nil -*- */
+// vim: ft=cpp:expandtab:ts=8:sw=4:softtabstop=4:
+
+#ifndef BACKUP_INTERNAL_H
+#define BACKUP_INTERNAL_H
+
+#include "backup.h"
+
+#ident "Copyright (c) 2012-2013 Tokutek Inc.  All rights reserved."
+#ident "$Id: backup.h 54696 2013-03-25 20:59:02Z bkuszmaul $"
+
+extern "C" {
+    unsigned long get_throttle(void);
+}
+
+#endif // end of header guardian.
