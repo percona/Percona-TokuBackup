3d624815c9720c58769fdb1e64320c96ee160784
Bradley C. Kuszmaul <bradley@tokutek.com>
2013-03-29 16:43:41 +0000

    Fixes #6410.  Add soname-synonyms to valgrind command line.

--- DartConfig.cmake
+++ DartConfig.cmake
@@ -1,2 +1,2 @@
 set(MEMORYCHECK_COMMAND /usr/bin/valgrind)
-set(MEMORYCHECK_COMMAND_OPTIONS "--error-exitcode=1 --suppressions=${PROJECT_SOURCE_DIR}/valgrind.suppressions --gen-suppressions=no --quiet --num-callers=20 --leak-check=full --show-reachable=yes" CACHE INTERNAL "options for valgrind")
+set(MEMORYCHECK_COMMAND_OPTIONS "--error-exitcode=1 --soname-synonyms=somalloc=*tokuportability* --suppressions=${PROJECT_SOURCE_DIR}/valgrind.suppressions --gen-suppressions=no --quiet --num-callers=20 --leak-check=full --show-reachable=yes" CACHE INTERNAL "options for valgrind")
