7831b3b05191e988ec3b1779f106e04e9d12d682
Leif Walsh <leif.walsh@gmail.com>
2013-04-08 21:01:36 +0000

    include CTest and remove extra settings in CTestCustom

--- CTestCustom.cmake
+++ CTestCustom.cmake
@@ -19,7 +19,3 @@ foreach(test
       drd/${test}
       )
   endforeach(test)
-
-set(MEMORYCHECK_COMMAND /usr/bin/valgrind)
-set(MEMORYCHECK_SUPRESSIONS_FILE "${PROJECT_SOURCE_DIR}/valgrind.suppressions")
-set(MEMORYCHECK_COMMAND_OPTIONS "--error-exitcode=1 --soname-synonyms=somalloc=*tokuportability* --gen-suppressions=no --quiet --num-callers=20 --leak-check=full --show-reachable=yes" CACHE INTERNAL "options for valgrind")
