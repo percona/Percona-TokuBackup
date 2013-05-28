c08781e7838c0a924d3a1f8a095ea30002a44a99
Bradley C. Kuszmaul <bradley@tokutek.com>
2013-05-28 15:37:46 -0400

    #1 Add errno and add a facility for better backtracing

--- backup/check.h
+++ backup/check.h
@@ -7,10 +7,13 @@
 #ident "Copyright (c) 2012-2013 Tokutek Inc.  All rights reserved."
 #ident "$Id$"
 
-void check_fun(long predicate, const char *expr, const char *file, int line) throw();
+#include "backtrace.h"
+
+void check_fun(long predicate, const char *expr, const backtrace bt) throw();
 
 // Like assert, except it doesn't go away under NDEBUG.
 // Do this with a function so that we don't get false answers on branch coverage.
-#define check(x) check_fun((long)(x), #x, __FILE__, __LINE__)
+#define check(x) check_fun((long)(x), #x, backtrace(__FILE__, __LINE__, __FUNCTION__, NULL))
+#define check_bt(x, bt) check_fun((long)(x), #x, backtrace(FILE, __LINE__, __FUNCTION__, bt))
 
 #endif
