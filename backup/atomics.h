2e8fc5df8cca8994d08bc6d3c39a24629053dd14
Bradley C. Kuszmaul <bradley@tokutek.com>
2013-06-06 11:27:01 -0400

    Make the branch work with gcc or the threadsanitizer.  #40.

--- backup/atomics.h
+++ backup/atomics.h
@@ -1,20 +1,34 @@
 template <class T> static T atomic_load_weak(volatile T *p) {
+#ifdef __clang__
     T v;
     __atomic_load(p, &v, __ATOMIC_RELAXED);
     return v;
+#else
+    return *p;
+#endif
 }
 template <class T> static T atomic_load_strong(volatile T *p) {
+#ifdef __clang__
     T v;
     __atomic_load(p, &v, __ATOMIC_SEQ_CST);
     return v;
+#else
+    return *p; // gcc 4.7.2 doesn't like that atomic load.
+#endif
 }
 
-template <class T> static T atomic_store_weak(volatile T *p, T v) {
+template <class T> static void atomic_store_weak(volatile T *p, T v) {
+#ifdef __clang__
     __atomic_store(p, &v, __ATOMIC_SEQ_CST);
-    return v;
+#else
+    *p = v;
+#endif
 }
 
-template <class T> static T atomic_store_strong(volatile T *p, T v) {
+template <class T> static void atomic_store_strong(volatile T *p, T v) {
+#ifdef __clang__
     __atomic_store(p, &v, __ATOMIC_SEQ_CST);
-    return v;
+#else
+    *p = v;
+#endif
 }
