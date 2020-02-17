percona/Percona-TokuBackup

commit 0ab06b5ba05d0cb4183326599eb47f898b8cccf3

lp1738417: Fixing a clang 5 build error
read, and some other functions are defined at multiple locations:
* in unistd.h
* in backup.cc

This causes a build error with clang5 and RelWithDebInfo. For some
reason, other build variants/compiler are unaffected, but as the
file in question didn't require anything from unistd, it can be
removed.

backup/backup.cc
@@ -36,7 +36,6 @@ Copyright (c) 2006, 2015, Percona and/or its affiliates. All rights reserved.
 
 #include <stdio.h> // rename(),
 #include <fcntl.h> // open()
-#include <unistd.h> // close(), write(), read(), unlink(), truncate(), etc.
 #include <sys/stat.h> // mkdir()
 #include <errno.h>
 #include <dlfcn.h>

commit be5c3c92a1549aef950a22756b7a0761c9cf70b1

The hot backup library no longer intercepts large file API calls. Revert commit 0ab06b5ba05d0cb4183326599eb47f898b8cccf3 which caused these APIs to not be defined correctly, which causes the hot backup library to NOT export the correct large file API.  If the library does not intercept these calls, the backup consistency is broken.

backup/backup.cc
@@ -36,6 +36,7 @@ Copyright (c) 2006, 2015, Percona and/or its affiliates. All rights reserved.
 
 #include <stdio.h> // rename(),
 #include <fcntl.h> // open()
+#include <unistd.h> // close(), write(), read(), unlink(), truncate(), etc.
 #include <sys/stat.h> // mkdir()
 #include <errno.h>
 #include <dlfcn.h>